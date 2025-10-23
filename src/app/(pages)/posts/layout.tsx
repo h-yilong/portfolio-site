9cbb88cd88820b4d2e5a1adbe768f814b3742c60
Yilong <huangyilong.au@gmail.com>
2025-10-23 23:23:54 +1100

    feat: add code syntax highlighting

--- src/app/(pages)/posts/layout.tsx
+++ src/app/(pages)/posts/layout.tsx
@@ -7,7 +7,7 @@ export default async function Layout({
 }>) {
   return (
     // tips: https://github.com/tailwindlabs/tailwindcss-typography?tab=readme-ov-file#element-modifiers
-    <main className="prose prose-base prose-headings:leading-[1.2] prose-headings:mb-4 prose-headings:text-indigo-400 prose-h3:font-bold prose-headings:mt-0 dark:prose-invert max-width prose-blockquote:text-amber-400 prose-p:my-4 prose-blockquote:bg-white/5 mx-auto mt-16 pt-6 font-medium [&_img]:mx-auto [&_img]:w-full [&_img]:max-w-5xl [&_img]:rounded-md [&_pre]:font-mono [&_pre]:text-sm">
+    <main className="prose prose-base prose-headings:leading-[1.2] prose-pre:bg-[#223] prose-headings:mb-4 prose-headings:text-indigo-400 prose-h3:font-bold prose-headings:mt-0 dark:prose-invert prose-code:font-medium prose-code:text-sm prose-blockquote:text-amber-400 prose-p:my-4 prose-blockquote:bg-white/5 mx-auto mt-16 pt-6 font-medium [&_img]:mx-auto [&_img]:w-full [&_img]:rounded">
       {children}
     </main>
   );
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,30 +1,35 @@
 import type { MDXComponents } from "mdx/types";
-// import Image, { ImageProps } from "next/image";
+import Image, { ImageProps } from "next/image";
+import { highlight } from "sugar-high";
 
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
 // React component you want, including inline styles,
 // components from other libraries, and more.
 
-// const components = {
-//   // Allows customizing built-in components, e.g. to add styling.
-//   h1: ({ children }) => <h1 style={{ color: "red", fontSize: "48px" }}>{children}</h1>,
-//   img: (props) => (
-//     <Image
-//       sizes="100vw"
-//       style={{ width: "100%", height: "auto" }}
-//       width={800}
-//       height={800}
-//       quality={90}
-//       loading="lazy"
-//       crossOrigin="anonymous"
-//       {...(props as ImageProps)}
-//       alt={props.alt ?? ""}
-//     />
-//   ),
-// } satisfies MDXComponents;
+const components = {
+  // Allows customizing built-in components, e.g. to add styling.
+  // h1: ({ children }) => <h1 style={{ color: "red", fontSize: "48px" }}>{children}</h1>,
+  code: ({ children, ...props }) => {
+    const codeHtml = highlight(children);
+    return <code dangerouslySetInnerHTML={{ __html: codeHtml }} {...props} />;
+  },
+  img: (props) => (
+    <Image
+      // sizes="100vw"
+      style={{ width: "100%", height: "auto" }}
+      width={900}
+      height={900}
+      quality={90}
+      loading="lazy"
+      crossOrigin="anonymous"
+      {...(props as ImageProps)}
+      alt={props.alt ?? ""}
+    />
+  ),
+} satisfies MDXComponents;
 
-const components = {};
+// const components = {};
 
 export function useMDXComponents(): MDXComponents {
   return components;
